e7f4a1f8c243b4eb6bc567bbb73cc1f06b5c4c89
Erick Araújo <araujoerick.dev@gmail.com>
2024-11-22 02:47:38 -0300

    :sparkles: feat: add function to add task

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const inputElement = document.querySelector(".new-task-input");
 const addTaskButton = document.querySelector(".new-task-button");
+const tasksContainer = document.querySelector(".tasks-container");
 
 const validateInput = () => inputElement.value.trim().length > 0;
 
@@ -9,6 +10,24 @@ const handleAddTask = () => {
   if (!inputIsValid) {
     return inputElement.classList.add("error");
   }
+
+  const taskItemContainer = document.createElement("div");
+  taskItemContainer.classList.add("task-item");
+  const taskContent = document.createElement("p");
+  taskContent.innerText = inputElement.value;
+
+  const deleteItem = document.createElement("button");
+  deleteItem.classList.add("delete-item");
+  const deleteIcon = document.createElement("img");
+  deleteIcon.setAttribute("src", "./assets/img/delete-icon.svg");
+  deleteItem.appendChild(deleteIcon);
+
+  taskItemContainer.appendChild(taskContent);
+  taskItemContainer.appendChild(deleteItem);
+
+  tasksContainer.appendChild(taskItemContainer);
+
+  inputElement.value = "";
 };
 
 const handleInputChange = () => {
